76d748543b22e599a071830523a16d4e74075176
Olga Albukova <o.albukova@gmail.com>
2020-04-15 00:18:23 +0300

    fix bag

--- script.js
+++ script.js
@@ -9,32 +9,27 @@ let jobInput = document.querySelector(".popup__imput_type_job");
 const formElement = document.querySelector(".popup__container");
 const savePopup = document.querySelector(".popup__button_type_save");
 
-function Add() {
+function open() {
   let profileTitleName = profileTitle.textContent;
   let profileTitleJob = profileSubtitle.textContent;
   nameInput.value = `${profileTitleName}`;
   jobInput.value = `${profileTitleJob}`;
-}
-
-function open() {
   popup.classList.add("popup_opened");
-  Add();
 }
-editButton.addEventListener("click", open);
 
 function close() {
   popup.classList.remove("popup_opened");
 }
-closePopup.addEventListener("click", close);
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-
   let nameInputAdd = nameInput.value;
   let jobInputAdd = jobInput.value;
-
   profileTitle.textContent = `${nameInputAdd}`;
   profileSubtitle.textContent = `${jobInputAdd}`;
+  close()
 }
+
+editButton.addEventListener("click", open);
+closePopup.addEventListener("click", close);
 formElement.addEventListener("submit", formSubmitHandler);
-savePopup.addEventListener("click", close);
