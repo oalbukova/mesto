9979be323f81a1768711ed6441b4fe9c8aadbc23
Olga Albukova <o.albukova@gmail.com>
2020-05-02 00:52:17 +0300

    first

--- script.js
+++ script.js
@@ -1,44 +1,40 @@
-const popup = document.querySelector(".popup");
 const formElement = document.querySelector(".popup__container"); //форма попап профиль
 const form = document.querySelector("#form"); //форма попап картинки
-const popupView = document.querySelector(".popup-view"); //форма попап картинки увеличенной
-const openPopup = document.querySelector(".popup_opened");
 const popupProfile = document.querySelector("#popupProfile"); //id попап профиль
 const popupElements = document.querySelector("#popupElements"); //id попап картинки
 const popupBig = document.querySelector("#popupBig"); //id попап большой картинки
 const editButton = document.querySelector(".profile__edit-button"); //кнопка открытия формы профиль
 const closePopup = document.querySelector(".button-close"); //кнопка закрытия формы профиль
-const savePopup = document.querySelector(".popup__button-save"); //кнопка сохранить формы профиль
 const addButton = document.querySelector(".profile__add-button"); //кнопка открытия формы картинок
 const elemBtnClose = document.getElementById("elements-close"); //кнопка закрытия формы картинок
 const elementsContainer = document.querySelector(".elements");
 const elementTemplate = document.getElementById("element-template");
 const viewClose = document.querySelector("#view-close"); //кнопка закрытия попап увеличенной картинки
-let viewLink = document.querySelector(".popup-view__img"); // картинка в попап
-let viewCaption = document.querySelector(".popup-view__caption"); //название увеличенной картинки
-let profileTitle = document.querySelector(".profile__title"); //имя на странице
-let profileSubtitle = document.querySelector(".profile__subtitle"); //профессия на странице
-let profileAlt = document.querySelector(".profile__img"); //alt в профиле
-let nameInput = document.querySelector(".popup__imput_type_name"); //имя в инпут
-let jobInput = document.querySelector(".popup__imput_type_job"); //профессия в инпут
-let placeInput = document.querySelector(".popup__imput_type_place"); //имя в инпут
-let linkInput = document.querySelector(".popup__imput_type_link"); //линк в инпут
+const viewLink = document.querySelector(".popup-view__img"); // картинка в попап
+const viewCaption = document.querySelector(".popup-view__caption"); //название увеличенной картинки
+const profileTitle = document.querySelector(".profile__title"); //имя на странице
+const profileSubtitle = document.querySelector(".profile__subtitle"); //профессия на странице
+const profileAlt = document.querySelector(".profile__img"); //alt в профиле
+const nameInput = document.querySelector(".popup__input_type_name"); //имя в инпут
+const jobInput = document.querySelector(".popup__input_type_job"); //профессия в инпут
+const placeInput = document.querySelector(".popup__input_type_place"); //имя в инпут
+const linkInput = document.querySelector(".popup__input_type_link"); //линк в инпут
 
 function editForm(elem) {
   elem.classList.toggle('popup_opened')
-  let profileTitleName = profileTitle.textContent;
-  let profileTitleJob = profileSubtitle.textContent;
+  const profileTitleName = profileTitle.textContent;
+  const profileTitleJob = profileSubtitle.textContent;
   nameInput.value = `${profileTitleName}`;
   jobInput.value = `${profileTitleJob}`;
 }
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  let nameInputAdd = nameInput.value;
-  let jobInputAdd = jobInput.value;
-  profileTitle.textContent = `${nameInputAdd}`;
-  profileSubtitle.textContent = `${jobInputAdd}`;
-  profileAlt.alt = `${nameInputAdd}`;
+  const nameInputAdd = nameInput.value;
+  const jobInputAdd = jobInput.value;
+  profileTitle.textContent = nameInputAdd;
+  profileSubtitle.textContent = jobInputAdd;
+  profileAlt.alt = nameInputAdd;
   editForm(popupProfile);
 }
 
