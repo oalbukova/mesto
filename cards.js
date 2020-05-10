f9e06fb34903fde71a4998a284f260146143c73d
Olga Albukova <o.albukova@gmail.com>
2020-05-10 03:17:58 +0300

    change img

--- cards.js
+++ cards.js
@@ -1,25 +1,25 @@
 const initialCards = [{
-  name: 'Архыз',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  name: 'Кавказ',
+  link: './images/kavkaz.jpg',
 },
 {
-  name: 'Челябинская область',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  name: 'Крым',
+  link: './images/crimea.jpg',
 },
 {
-  name: 'Иваново',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  name: 'Хребет Нургуш',
+  link: './images/nuegush.jpg',
+},
+{
+  name: 'Токсово',
+  link: './images/toksovo.jpg',
 },
 {
   name: 'Камчатка',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-},
-{
-  name: 'Холмогорский район',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
 },
 {
   name: 'Байкал',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  link: './images/baykal.jpg',
 }
 ];
